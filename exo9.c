6f1616247964cd351551c78bf557e55acf30d2fa
Khaoussou <111887621+Khaoussou@users.noreply.github.com>
2023-02-02 02:48:59 +0000

    Add files via upload

--- exo9.c
+++ exo9.c
@@ -1,1 +1,13 @@
-
+#include<stdio.h>
+main()
+{
+    int n,N,i;
+    n=50;
+    printf("Donner un nombre: \n");
+    scanf("%d",&N);
+    while(N<n)
+    {
+        printf("Donner un nombre: \n");
+        scanf("%d",&N);
+    }
+}
