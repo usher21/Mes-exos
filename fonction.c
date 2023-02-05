a5822466a03da2c1a32961e44f7ca7ac279cdd23
Khaoussou <111887621+Khaoussou@users.noreply.github.com>
2023-02-05 03:29:15 +0000

    Update fonction.c

--- fonction.c
+++ fonction.c
@@ -50,3 +50,40 @@ int date_valide (int jr,int m,int a)
         
     return jour;
 }
+
+int date_suivante(int jr,int m,int an)
+{
+    int jour,mois,a;
+
+        if(date_valide(jr,m,an))
+        {
+            if(m == 2)
+            {
+                jour=1;
+                mois=3;
+            }
+            else
+            {
+                jour=29;
+            }
+            if( m == 12 && jr == 31)
+            {
+                jour=1;
+                mois=1;
+                a++;
+            }
+            else if (m == 4 || m == 6 || m == 9 || (m == 11 && jr == 30))
+            {
+                jour=1;
+                mois++;
+            }
+            else if (m == 3 || m == 5 || m == 7 || m == 8 || (m == 10 && jr == 31))
+            {
+                jour=1;
+                mois++;
+            }
+            else
+                jour++;     
+        }
+     return jour,mois,a; 
+}
